519f9394e942452610b7e85334dfeb15096ff006
Xwvike <26395177+XWVIKE@users.noreply.github.com>
2018-09-01 18:19:04 +0800

    temp

--- sort/drawing.js
+++ sort/drawing.js
@@ -1,10 +1,13 @@
 const drawing = document.getElementById("box");
 const context = drawing.getContext("2d");
-let x = 0, y = 500;
-const arr = Object.keys(Array.from({length: 500})).map(function (item) {
+let x = 0,
+    y = 100;
+const arr = Object.keys(Array.from({
+    length: 100
+})).map(function (item) {
     return +item;
 });
-let newArr = FisherYatesShuffle(arr);
+
 function FisherYatesShuffle(arr) {
     for (let i = 1; i < arr.length; i++) {
         const random = Math.floor(Math.random() * (i + 1));
@@ -13,26 +16,25 @@ function FisherYatesShuffle(arr) {
     return arr;
 }
 
-function bubbleSort (arr) {
+let newArr = FisherYatesShuffle(arr);
+
+
+function bubbleSort(arr) {
     let length = arr.length;
     for (let i = 0; i < length; i++) {
         for (let j = 0; j < length - 1; j++) {
             if (arr[j] > arr[i]) {
-                swap(arr,j,j+1)
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
         }
     }
 }
-function drawLine(context, x, y,width,color,time) {
-    setTimeout(function () {
-        context.beginPath();
-        context.save();
-        context.strokeStyle = color;
-        context.lineWidth = width;
-        context.moveTo(x*width,y);
-        context.lineTo(x,y);
-        context.restore();
-    },time)
+
+function drawLine(context) {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    for (let i = 0; i < newArr.length; i++) {
+        if ()
+            }
 
 }
 
@@ -41,16 +43,10 @@ function draw() {
     context.fillStyle = "#000";
     context.fillRect(0, 0, 1500, 500);
     for (let i = 0; i < newArr.length; i++) {
-        drawLine(context,x,y-arr[i],3,"#fff",0);
-        x+=3;
+        drawLine(context, x, y - arr[i], 3, "#fff", 0);
+        x += 3;
     }
 }
-function swap(array,index1,index2){
-    let aux = array[index1];
-    array[index1] = array[index2];
-    drawLine(context,index1,y-array[index1],3,"#fff",0);
-    array[index2] = aux;
-    drawLine(context,index2,y-array[index2],3,"#fff",0);
-}
+
 draw();
 bubbleSort(newArr);
